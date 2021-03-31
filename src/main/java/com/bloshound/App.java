6be93434b0561823441ab8109c5099224f02b6ad
bloshound <bloshound@gmail.com>
2021-03-31 15:31:44 +0300

    aa

--- src/main/java/com/bloshound/App.java
+++ src/main/java/com/bloshound/App.java
@@ -1,20 +1,14 @@
 package com.bloshound;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-        HttpURLConnection connection;
 
-
-
+        HtmlDownloader htmlDownloader = new HtmlDownloader("https://google.com");
+        htmlDownloader.downloadHtml();
     }
 }
