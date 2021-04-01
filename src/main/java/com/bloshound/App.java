6312389aba991d5b98a7c312bb07eda26a7f8ea4
bloshound <bloshound@gmail.com>
2021-04-01 23:19:34 +0300

    !!!

--- src/main/java/com/bloshound/App.java
+++ src/main/java/com/bloshound/App.java
@@ -4,26 +4,24 @@ package com.bloshound;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * Hello world!
- */
+
+
 public class App {
     public static void main(String[] args) throws IOException {
 
-        HTMLDownloader htmlDownloader = new HTMLDownloader("https://yandex.ru/search/?clid=2186618&text=string.split%20java&lr=42&redircnt=1617225411.1");
-        String dawnloadedHtml = htmlDownloader.downloadHtml();
+        HTMLDownloader htmlDownloader = new HTMLDownloader("https://yandex.ru");
+        String downloadedHtml = htmlDownloader.downloadHtml();
 
-        Document doc = HTMLParser.parse(dawnloadedHtml);
+        Document doc = HTMLParser.parse(downloadedHtml);
         String text = HTMLParser.text(doc);
 
         Util util = new Util();
-        Map<String, Long> entrances = util.findDoublicates(text);
+        Map<String, Long> entrances = util.entrancesCounting(text);
 
-        LinkedHashMap<String, Long> sortedEntrances = util.sortbyValueLinked(entrances);
+        LinkedHashMap<String, Long> sortedEntrances = util.sortedLinkedEntrancesCounting(entrances);
 
         System.out.println(sortedEntrances);
 
--- src/main/java/com/bloshound/Util.java
+++ src/main/java/com/bloshound/Util.java
@@ -2,36 +2,53 @@ package com.bloshound;
 
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Util {
 
-    public static final char[] delimiters = {' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t'};
+    // public static final char[] delimiters = {' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t'};
+
     private String regex = "[ ,.!?\";:\\[\\]()\n\r\t]+";
+
+    public Util(String regex) {
+        Objects.requireNonNull(regex);
+        this.regex = regex;
+    }
+
+    public Util() {
+    }
 
     public String getRegex() {
         return regex;
+
+    }
+
+    public void setRegex(String regex) {
+        Objects.requireNonNull(regex);
+        this.regex = regex;
     }
 
     private String[] splitOnDelimeters(String content) {
         return content.trim().split(regex);
     }
 
-    public Map<String, Long> findDoublicates(String content) {
+    public Map<String, Long> entrancesCounting(String content) {
         Map<String, Long> entrances = Arrays.stream(splitOnDelimeters(content))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return entrances;
     }
 
-    public LinkedHashMap<String, Long> sortbyValueLinked(Map<String, Long> map) {
+    public LinkedHashMap<String, Long> sortedLinkedEntrancesCounting(Map<String, Long> map) {
         LinkedHashMap<String, Long> sortedEntrances = map.entrySet()
                 .stream()
                 .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, ((oldvalue, newvalue) -> newvalue), LinkedHashMap::new));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> newValue, LinkedHashMap::new));
         return sortedEntrances;
     }
+
+
 }
